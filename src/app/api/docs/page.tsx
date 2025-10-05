55e48e342b2c43d7f11c6d5e826e556c1db3eec3
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2025-10-05 16:41:46 +0000

    fixed docs page theme

--- src/app/api/docs/page.tsx
+++ src/app/api/docs/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { RedocStandalone } from "redoc";
 
 export default function ApiDocsPage() {
   const [mounted, setMounted] = useState(false);
+  const { setTheme } = useTheme();
 
   useEffect(() => {
     setMounted(true);
+    setTheme("light");
   }, []);
 
   if (!mounted) {
